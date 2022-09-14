b4711a1811f4069936e6024c7a2555ebcd74c309
Bruno Guimaraes <devgmrs@gmail.com>
2022-09-14 20:37:08 -0300

    :lipstick:	feat: acerto no padding da logo no nav

--- src/assets/Pages/Cardapio/index.jsx
+++ src/assets/Pages/Cardapio/index.jsx
@@ -4,7 +4,7 @@ import logo from '../../logo.svg';
 export default function Cardapio() {
 	return (
 		<main>
-			<nav className="px-[250px]">
+			<nav className="px-[250px] py-5">
 				<SVG src={logo} title="Logo Aluroni" />
 			</nav>
 		</main>
