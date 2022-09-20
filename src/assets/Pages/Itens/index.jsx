771d4b59b4f92253d6f7931aa3cf3246224a9bf5
Bruno Guimaraes <devgmrs@gmail.com>
2022-09-20 20:06:36 -0300

    :rocket: finalizacao do projeto

--- src/assets/Pages/Itens/index.jsx
+++ src/assets/Pages/Itens/index.jsx
@@ -1,10 +1,47 @@
+import { useEffect, useState } from 'react';
 import Item from './item';
 import cardapio from './itens.json';
 
-export default function Itens() {
+export default function Itens(props) {
+	const [lista, setlista] = useState(cardapio);
+	const { busca, filtro, ordenador } = props;
+
+	function testarBusca(params) {
+		const regex = new RegExp(busca, 'i');
+		return regex.test(params);
+	}
+
+	function testaFiltro(params) {
+		if (filtro !== null) return filtro === params;
+		return true;
+	}
+
+	function ordernar(params) {
+		switch (ordenador) {
+			case 'porcao':
+				return params.sort((a, b) => (a.size > b.size ? 1 : -1));
+
+			case 'qtd_pessoas':
+				return params.sort((a, b) => (a.serving > b.serving ? 1 : -1));
+
+			case 'preco':
+				return params.sort((a, b) => (a.price > b.price ? 1 : -1));
+
+			default:
+				return params;
+		}
+	}
+
+	useEffect(() => {
+		const newList = cardapio.filter(
+			(item) => testarBusca(item.title) && testaFiltro(item.category.id)
+		);
+		setlista(ordernar(newList));
+	}, [busca, filtro, ordenador]);
+
 	return (
 		<div className="flex flex-col flex-wrap gap-10 w-full">
-			{cardapio.map((itens) => (
+			{lista.map((itens) => (
 				<ul key={itens.id} className="">
 					<li className="">
 						<Item key={itens.id} itens={itens} />
