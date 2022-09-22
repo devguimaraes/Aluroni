e7638a04f4b6d6c04914f5f72c19452ca35f4972
Bruno Guimaraes <devgmrs@gmail.com>
2022-09-21 23:47:51 -0300

    :recycle:	refactor: Acerto no caminho de rotas para renderizacao condicional do Header

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,15 +4,18 @@ import Home from './assets/Home';
 import Cardapio from './assets/Pages/Cardapio';
 import Header from './Components/Header';
 import Menu from './Components/Menu';
+import Page4040 from './Components/Page404';
 
 export default function AppRouter() {
 	return (
 		<Router>
 			<Menu />
-			<Header />
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/cardapio" element={<Cardapio />} />
+				<Route path="/" element={<Header />}>
+					<Route index element={<Home />} />
+					<Route path="/cardapio" element={<Cardapio />} />
+				</Route>
+				<Route path="*" element={<Page4040 />} />
 			</Routes>
 		</Router>
 	);
